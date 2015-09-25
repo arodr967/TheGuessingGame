eb343f5c2db5751bf17cb8f1a9961fc12dd7ca3e
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-24 22:26:16 -0400

    Initial Commit

---
+++ The Guessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Guessing Game
+//
+//  Created by Alicia Rodriguez on 9/24/15.
+//  Copyright © 2015 Alicia Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
